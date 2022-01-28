92a2b11d999ff221fc7601345bef17ebaeb6f453
itshniki121 <dmitriy_gerasimov_02@mail.ru>
2022-01-28 19:56:02 +0300

    Создание

---
+++ Untitled-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+float a, b, c;
+char d;
+int p;
+int main()
+{//Подключение русского языка в программе
+    setlocale(LC_ALL, "Russian");                              
+    while (p != 2)
+    {
+        cout << "1 = Calculator \n 2 - Otvet \n";
+        cin >> p;
+        switch (p)
+        {
+        case 1:
+        {
+            cout << "First ";
+            cin >> a;
+            cout << "Знак ";
+            cin >> d;
+            cout << "SECOND ";
+            cin >> b;
+            if (d == '+')
+                c = a + b;
+            if (d == '-')
+                c = a - b;
+            if (d == '*')
+                c = a * b;
+            if (d == '/')
+                c = a / b;
+        }
+        case 2:
+        {
+            break;
+        }
+        }
+        cout << "Ответ =" << c << endl;
+    }
+}
